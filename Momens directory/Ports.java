211a4d6b23e0330f127189b56b05fcf3f9d74a79
Momen Katba bader <momen10@unm.edu>
2023-04-16 21:43:04 +0000

    Upload New File

---
+++ Momens directory/Ports.java
@@ -0,0 +1,18 @@
+public class Ports {
+
+    int outport;
+    public Ports(){
+
+        outport = 0;
+    }
+
+
+    public int getOutport() {
+        return outport;
+    }
+
+    public void setOutport(int outport) {
+        this.outport = outport;
+    }
+}
+

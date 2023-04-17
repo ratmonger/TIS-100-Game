b229baf0fa0ab4897b5e195515df4c9a17d5c8f6
Bryce Palmer <dunharrow@unm.edu>
2023-04-16 23:22:33 -0600

    Mario can really JUMP!

--- untitled/src/Interpreter.java
+++ untitled/src/Interpreter.java
@@ -11,11 +11,12 @@ public class Interpreter implements Runnable {
     private Port rightPort;
     static int counter = 0;
     private final CountDownLatch latch;
-     int index = 0;
-     int count = 0;
+    int index = 0;
+    int count = 0;
 
 
-    public Interpreter(Port upPort, Port downPort, Port leftPort, Port rightPort, CountDownLatch latch, int index) {
+    public Interpreter(Port upPort, Port downPort, Port leftPort,
+                       Port rightPort, CountDownLatch latch, int index) {
         this.upPort = upPort;
         this.downPort = downPort;
         this.leftPort = leftPort;
@@ -28,14 +29,68 @@ public class Interpreter implements Runnable {
     @Override
     public void run() {
         Parcer parcer = new Parcer();
-        while (true){
-            if(count>parcer.getinstruct().get(index).size()-1)
-                count=0;
+        while (true) {
+            if (count > parcer.getinstruct().get(index).size() - 1)
+                count = 0;
             String word = (String) parcer.getinstruct().get(index).get(count);
             Scanner sc = new Scanner(word);
+            int srcint = 0;
+            int dstint = 0;
+            String src;
+            String dst;
+            String label;
             switch (sc.next()) {
+                case "JRO":
+                    src = sc.next();
+                    srcint = srcCase(src);
+                    count = ((count + srcint) %
+                            parcer.getinstruct().get(index).size()) - 1;
+                    break;
+                case "JUMP":
+                    label = sc.next();
+                    count =
+                            parcer.getinstruct().get(index).indexOf(":"+label+":") -1;
+                    // jump to label? or jump to instruction after label?
+                    // remove the -1 to jump to instruction asap
+
+                    // this means we stall for one turn or we immediately
+                    // resume instructions
+                    break;
+
+
+
+                case "JEZ":
+                    label = sc.next();
+                    if (ACC == 0) {
+                        count = parcer.getinstruct().get(index).indexOf(":"+label+":") -
+                                1;
+                    }
+
+                case "JNZ":
+                    label = sc.next();
+                    if (ACC != 0) {
+                        count = parcer.getinstruct().get(index).indexOf(":"+label+":") -
+                                1;
+                    }
+
+                case "JGZ":
+                    label = sc.next();
+                    if (ACC > 0) {
+                        count = parcer.getinstruct().get(index).indexOf(":"+label+":") -
+                                1;
+                    }
+
+                case "JLZ":
+                    label = sc.next();
+                    if (ACC < 0) {
+                        count = parcer.getinstruct().get(index).indexOf(":"+label+":") -
+                                1;
+                    }
+
                 case "ADD":
-                    ACC = ACC + sc.nextInt();
+                    src = sc.next();
+                    srcint = srcCase(src);
+                    ACC += srcint;
                     break;
                 case "SWAP":
                     int temp = ACC;
@@ -43,98 +98,19 @@ public class Interpreter implements Runnable {
                     BAK = temp;
                     break;
                 case "NEGATE":
-                    ACC = ACC *-1;
+                    ACC = ACC * -1;
                     break;
                 case "NOOP":
                     break;
                 case "SUB":
-                    ACC = ACC - sc.nextInt();
+                    src = sc.next();
+                    srcint = srcCase(src);
+                    ACC -= srcint;
                     break;
                 case "MOVE":
-                    int srcint = 0;
-                    int dstint = 0;
-                    String src = sc.next();
-                    String dst = sc.next();
-                    switch (src) {
-                        case "UP":
-                            if (upPort instanceof InputPort) {
-                                try {
-                                    srcint =upPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            } else if (upPort instanceof OutPort) {
-                                // upPort is an output port
-                            } else {
-                                try {
-                                    latch.countDown();
-                                    srcint = upPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                            break;
-                        case "DOWN":
-                            if (downPort instanceof InputPort) {
-                                try {
-                                    srcint =downPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            } else if (upPort instanceof OutPort) {
-                                // upPort is an output port
-                            } else {
-                                try {
-                                    latch.countDown();
-                                    srcint = downPort.getOutQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                            break;
-                        case "LEFT":
-                            if (leftPort instanceof InputPort) {
-                                try {
-                                    srcint =leftPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            } else if (upPort instanceof OutPort) {
-                                // upPort is an output port
-                            } else {
-                                try {
-                                    latch.countDown();
-                                    srcint = leftPort.getOutQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                            break;
-                        case "RIGHT":
-                            if (rightPort instanceof InputPort) {
-                                try {
-                                    srcint =rightPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            } else if (upPort instanceof OutPort) {
-                                // upPort is an output port
-                            } else {
-                                try {
-                                    latch.countDown();
-                                    srcint = rightPort.getInQueue().take();
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                            break;
-                        case "ACC":
-                            srcint=ACC;
-                            break;
-                        case "BAK":
-                            srcint=BAK;
-                            break;
-                    }
+                    src = sc.next();
+                    dst = sc.next();
+                    srcint = srcCase(src);
 
                     switch (dst) {
                         case "UP":
@@ -168,19 +144,25 @@ public class Interpreter implements Runnable {
                         case "ACC":
                             ACC = srcint;
                             break;
-                        case "BAK":
-                            BAK = srcint;
+                        case "NIL":
                             break;
+                        //case "BAK":
+                        //   BAK = srcint;
+                        //  break;
                     }
-                    break;
 
+
+                    break;
                 case "SAVE":
                     BAK = ACC;
                     break;
+                default: //label here, do nothing?
             }
 
             count++;
-            System.out.println(Thread.currentThread().getName()+" ACC:" + ACC + " BAK:"+ BAK);
+            System.out.println(
+                    Thread.currentThread().getName() + " ACC:" + ACC + " BAK:" +
+                            BAK);
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -194,6 +176,94 @@ public class Interpreter implements Runnable {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public int srcCase(String src) {
+        int srcint = 0;
+        switch (src) {
+            case "UP":
+                if (upPort instanceof InputPort) {
+                    try {
+                        srcint = upPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (upPort instanceof OutPort) {
+                    // upPort is an output port
+                } else {
+                    try {
+                        latch.countDown();
+                        srcint = upPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            case "DOWN":
+                if (downPort instanceof InputPort) {
+                    try {
+                        srcint = downPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (upPort instanceof OutPort) {
+                    // upPort is an output port
+                } else {
+                    try {
+                        latch.countDown();
+                        srcint = downPort.getOutQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            case "LEFT":
+                if (leftPort instanceof InputPort) {
+                    try {
+                        srcint = leftPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (upPort instanceof OutPort) {
+                    // upPort is an output port
+                } else {
+                    try {
+                        latch.countDown();
+                        srcint = leftPort.getOutQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            case "RIGHT":
+                if (rightPort instanceof InputPort) {
+                    try {
+                        srcint = rightPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (upPort instanceof OutPort) {
+                    // upPort is an output port
+                } else {
+                    try {
+                        latch.countDown();
+                        srcint = rightPort.getInQueue().take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            case "ACC":
+                srcint = ACC;
+                break;
+            case "NIL":
+                srcint = 0;
+            default:
+                srcint = Integer.parseInt(src);
+        }
+        return srcint;
+
+
     }
 
 

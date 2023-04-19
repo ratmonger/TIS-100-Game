87e3a44d10a15f69a33a801d2c8036411400ac1a
Bryce Palmer <dunharrow@unm.edu>
2023-04-19 17:47:35 -0600

    fixed silos and highliting line

--- src/Silo.java
+++ src/Silo.java
@@ -39,20 +39,35 @@ public class Silo implements Component{
         }
         sil.setPrefSize(130, 130);
         String text = sil.getText();
+
+
         sil.setStyle(
-                "text-area-background: rgb(23, 23, 23);"+
-                        "-fx-focus-color: rgb(23, 23, 23) ; " +
-                        "-fx-faint-focus-color: rgb(23, 23, 23) ;"+
-                        "-fx-highlight-fill: rgb(23, 23, 23);" +
-                        "-fx-highlight-text-fill: white;"+
-                        "-fx-background-color: rgb(23, 23, 23);" +
-                        "-fx-control-inner-background: rgb(23, 23, 23); -fx-text-fill: white;");
+                "-fx-highlight-fill: black; " +
+                        "-fx-highlight-text-fill: white;" +
+                        "-fx-focus-color: transparent ; " +
+                        "-fx-faint-focus-color: transparent ;");
+
+
+
+//        sil.setStyle(
+//                "text-area-background: rgb(23, 23, 23);"+
+//                        "-fx-focus-color: rgb(23, 23, 23) ; " +
+//                        "-fx-faint-focus-color: rgb(23, 23, 23) ;"+
+//                        "-fx-highlight-fill: rgb(23, 23, 23);" +
+//                        //"-fx-highlight-text-fill: white;"+
+//                        "-fx-background-color: rgb(23, 23, 23);" +
+//                        "-fx-background-radius: 0;" +
+//                        "-fx-control-inner-background: rgb(23, 23, 23); "
+//                        //"-fx-text-fill: white;"
+//                         );
+
+
 
 //        +
 //                "-fx-highlight-fill: rgb(23, 23, 23); " +
 //                "-fx-highlight-text-fill: rgb(23, 23, 23);" +
 
-        sil.setBackground(Background.fill(Color.rgb(23, 23, 23)));
+        //sil.setBackground(Background.fill(Color.rgb(23, 23, 23)));
         // THIS IS WHERE WE WILL HIGHLIGHT
         // WE NEED TO GET THE RANGE HERE
         // WRITE A SILO METHOD THAT RETURNS THE RANGE!
@@ -66,21 +81,36 @@ public class Silo implements Component{
 
 
         VBox vbox = new VBox(acc,bak);
-        //vbox.setSpacing(100);
+        vbox.setSpacing(20);
 
 
         acc.setStyle(cssLayout);
         bak.setStyle(cssLayout);
-        acc.setTextFill(Paint.valueOf("WHITE"));
+        acc.setTextFill(Paint.valueOf("BLACK"));
+        acc.setBackground(Background.fill(Color.WHITE));
+        bak.setBackground(Background.fill(Color.WHITE));
         acc.setFont(new Font("Courier New Bold",
                              12));
-        bak.setTextFill(Paint.valueOf("WHITE"));
+        bak.setTextFill(Paint.valueOf("BLACK"));
         bak.setFont(new Font("Courier New Bold",
                              12));
         HBox hbox = new HBox(sil, vbox);
         BorderPane bp = new BorderPane(hbox);
+        //bp.setCenter(sil);
+        //bp.setRight(vbox);
         bp.setStyle(cssLayout);
-        bp.setMinSize(200,200);
+        bp.setMinSize(170,170);
+        bp.setBackground(Background.fill(Color.WHITE));
+
+        String cssLayout2 = "-fx-border-color: BLACK ;\n" +
+                "-fx-border-outsets: 5;\n" +
+                "-fx-border-insets: 3;\n"+
+                "-fx-border-width: 3;\n";
+
+        acc.setStyle(cssLayout2);
+        bak.setStyle(cssLayout2);
+
+        bp.setStyle(cssLayout2);
 
 
         return bp;

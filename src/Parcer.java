b893b2e96e3eb66663f2e76439f1bd080dc328a7
Bryce Palmer <dunharrow@unm.edu>
2023-04-19 19:29:21 -0600

    parcer error with empty silo

--- src/Parcer.java
+++ src/Parcer.java
@@ -138,6 +138,7 @@ public class Parcer {
 
 
 
+
     public void printarray(String[][] array){
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
